3d0d6ddec42d3399606ae5bd183306487bd3a8c5
Skye M <skyem@hotmail.co.uk>
2016-08-27 02:58:26 +0100

    work on the instructions

--- game/main.lua
+++ game/main.lua
@@ -1,7 +1,14 @@
 screen_displayed = nil
 
 function display_welcome()
-    love.graphics.print('Hello World!', 400, 300)
+    love.graphics.print(
+    "Welcome to our Ludum Dare game for Ludum Dare 36!\n" ..
+    "We haven't named it yet.\n\n" ..
+    "This game is about solving a puzzle from some ancient technology.\n" ..
+    "Not any old ancient technology, it's unrealistic ancient technolgy found in science fiction!\n" ..
+    "Anyway, link the crystals and you'll be fine, okay?\n" ..
+    "Good Luck!"
+    , 0, 0)
 end
 
 function love.draw()
