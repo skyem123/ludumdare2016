22d17b60819d3e41da64009fb24c8d415f2d91de
Skye M <skyem@hotmail.co.uk>
2016-08-27 02:43:18 +0100

    work on code

--- game/main.lua
+++ game/main.lua
@@ -1,3 +1,17 @@
-function love.draw()
+screen_displayed = nil
+
+function display_welcome()
     love.graphics.print('Hello World!', 400, 300)
+end
+
+function love.draw()
+    if screen_displayed ~= nil then
+        screen_displayed()
+        return;
+    end
+    -- the game should be rendered here.
+end
+
+function love.load()
+    screen_displayed = display_welcome
 end
