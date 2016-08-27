ca38a9038e3697eab3d0864fd269c5d5ad3ad4dd
Striking <Strikingwolf2012@gmail.com>
2016-08-27 17:44:01 -0500

    I forgot to push ffs self

--- game/crystal.lua
+++ game/crystal.lua
@@ -64,6 +64,23 @@ function Crystal:collision_check(x,y,w,h)
     return helpers.collision_check(self.x - s / 2, self.y - s / 2, s, s,  x,y,w,h)
 end
 
+function Crystal:orientation(q, r)
+  local val = (q.y - self.y) * (r.x - q.x) -
+              (q.x - self.x) * (r.y - q.y)
+  if val == 0 then
+    return 0
+  elseif (val > 0) then
+    return 1
+  else
+    return 2
+  end
+end
+
+function Crystal:onSeg(q, r)
+  return q.x <= math.max(self.x, r.x) and q.x >= math.min(self.x, r.x) and
+         q.y <= math.max(self.y, r.y) and q.y >= math.min(self.y, r.y)
+end
+
 end
 
 return Crystal
--- game/link.lua
+++ game/link.lua
@@ -29,38 +29,26 @@ function Link:draw()
 end
 
 function Link:collision_check(link)
-  return false -- TODO
-end
+  local o1 = self.c1.orientation(self.c2, link.c1)
+  local o2 = self.c1.orientation(self.c2, link.c2)
+  local o3 = link.c1.orientation(link.c2, self.c1)
+  local o4 = link.c1.orientation(link.c2, self.c2)
 
-function Link:solve_line()
-  local this = self
-  return function (x)
-    return this.slope() * (x - this.c1.x) + this.c1.y1
+  if (o1 ~= o2 and o3 ~= o4) then
+    return true
   end
-end
 
-function Link:same_line(link)
-  local function same_coord(a, b)
-    return a.x == b.x and a.y == b.y
-  end
-  return same_coord(self.c1, link.c1) and same_coord(self.c2, link.c2)
-end
-
-function Link:para_check(link)
-  return self.slope() == link.slope()
-end
-
-function Link:slope()
-  if (self.c1.x < self.c2.x) then
-    start = self.c1
-    target = self.c2
-  elseif (self.c1.x > self.c2.x) then
-    start = self.c2
-    target = self.c1
+  if (o1 == 0 and self.c1.onSeg(link.c1, self.c2)) then
+    return true
+  elseif (o2 == 0 and self.c1.onSeg(link.c2, self.c2)) then
+    return true
+  elseif (o3 == 0 and link.c1.onSeg(self.c1, link.c2)) then
+    return true
+  elseif (o4 == 0 and link.c1.onSeg(self.c2, link.c2)) then
+    return true
   else
-    return nil
+    return false
   end
-  return ((target.y - start.y) / (target.x - start.y))
 end
 
 return Link
